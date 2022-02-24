4a9587f7b5239fc606df4b3fba92259c622c0339
Noreh AD <akinoreh@gmail.com>
2022-02-24 19:18:51 +0300

    sub_dir

--- backup_project.sh
+++ backup_project.sh
@@ -6,9 +6,11 @@ date=$(TZ=Europe/Istanbul date +"%Y-%m-%d_%H-%M")
 # TODO: detect db
 db_vendor="mariadb"
 
-sudo /opt/bitnami/${db_vendor}/bin/mysqldump --user=root --password=${user_pass} ${db_name} > /home/bitnami/htdocs/dump_${date}.sql
+sub_dir=""
 
-cd /home/bitnami/htdocs
+sudo /opt/bitnami/${db_vendor}/bin/mysqldump --user=root --password=${user_pass} ${db_name} > /home/bitnami/htdocs${sub_dir}/dump_${date}.sql
+
+cd /home/bitnami/htdocs${sub_dir}
 zip -r htdocs_${date}.zip .
 
-rm /home/bitnami/htdocs/dump_${date}.sql
+rm /home/bitnami/htdocs${sub_dir}/dump_${date}.sql
